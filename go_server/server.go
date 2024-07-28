f8ab3e5e6ec3a5cb4ba9d07cbd6804d8bed456cd
HamnYce <hamadboa@gmail.com>
2024-07-28 16:21:57 +0300

    abstract out /infoon handler from main into function

--- go_server/server.go
+++ go_server/server.go
@@ -8,47 +8,48 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/infoon",
-		func(w http.ResponseWriter, r *http.Request) {
-			log.Print(r.URL.Query())
-			log.Print(r.Header)
-
-			w.Header().Del("Content-Type")
-			w.Header().Add("Content-Type", "application/json")
-
-			switch r.Method {
-			case http.MethodGet:
-				ips := strings.Split(r.URL.Query().Get("ips"), ",")
-
-				scanner, err := gomap.NewScanner(ips)
-
-				if err != nil {
-					log.Println(err)
-				}
-
-				_, jsn, warnings, err := gomap.ScanHosts(scanner)
-
-				if len(warnings) > 0 {
-					log.Println(warnings)
-				}
-
-				if err != nil {
-					log.Println(err)
-				}
-
-				_, err = w.Write(jsn)
-
-				if err != nil {
-					log.Print(err)
-				}
-
-			default:
-				log.Print("Method was not get request")
-				w.Write([]byte("{error: \"only accepting get requests\"}"))
-
-			}
-		})
+	http.HandleFunc("/infoon", infoOnHandler)
 
 	log.Fatal(http.ListenAndServe(":8020", nil))
 
+}
+
+func infoOnHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print(r.URL.Query())
+	log.Print(r.Header)
+
+	w.Header().Del("Content-Type")
+	w.Header().Add("Content-Type", "application/json")
+
+	switch r.Method {
+	case http.MethodGet:
+		ips := strings.Split(r.URL.Query().Get("ips"), ",")
+
+		scanner, err := gomap.NewScanner(ips)
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, jsn, warnings, err := gomap.ScanHosts(scanner)
+
+		if len(warnings) > 0 {
+			log.Println(warnings)
+		}
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, err = w.Write(jsn)
+
+		if err != nil {
+			log.Print(err)
+		}
+
+	default:
+		log.Print("Method was not get request")
+		w.Write([]byte("{error: \"only accepting get requests\"}"))
+
+	}
 }
