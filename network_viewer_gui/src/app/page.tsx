4129b8f658485fe4e1efbfe126c4edd5a86d0339
HamnYce <hamadboa@gmail.com>
2024-07-28 18:55:53 +0300

    add ports table in page rename src/components/host_tables/hostname_table.tsx to src/components/host_tables/hostnames_table.tsx

--- network_viewer_gui/src/app/page.tsx
+++ network_viewer_gui/src/app/page.tsx
@@ -13,15 +13,15 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import OSMatchesTable from "@/components/host_tables/os_matches_table";
-import HostNameTable from "@/components/host_tables/hostname_table";
+import HostNameTable from "@/components/host_tables/hostnames_table";
+import PortsTable from "@/components/host_tables/ports_table";
 
 export default function Home() {
   const [selectedHostIndex, setSelectedHostIndex] = useState(0);
 
-  // TODO: add buttons on the left panel to select between the different IPs
   return (
     <main className="flex h-screen w-full columns-2 bg-green-500">
-      <div className="flex-col min-h-screen bg-red-400 w-96 p-10 list-none">
+      <div className="flex-col min-h-screen bg-red-400 w-96 p-10 list-none overflow-auto">
         {hostData.map((host: Host, index: number) => {
           return (
             <h4
@@ -37,8 +37,9 @@ export default function Home() {
           );
         })}
       </div>
-      <div className="flex-col h-screen w-screen bg-blue-400 p-10">
+      <div className="flex-col h-screen w-screen bg-blue-400 p-10 overflow-auto">
         <HostNameTable host={hostData[selectedHostIndex]} />
+        <PortsTable host={hostData[selectedHostIndex]} />
         <OSMatchesTable host={hostData[selectedHostIndex]} />
       </div>
     </main>
