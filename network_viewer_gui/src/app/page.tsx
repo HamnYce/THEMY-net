c871b3211b77bc1ab3e2e3387af0fc7c01805c43
HamnYce <hamadboa@gmail.com>
2024-07-28 23:58:44 +0300

    change styling to dark mode

--- network_viewer_gui/src/app/page.tsx
+++ network_viewer_gui/src/app/page.tsx
@@ -20,16 +20,19 @@ export default function Home() {
   const [selectedHostIndex, setSelectedHostIndex] = useState(0);
 
   return (
-    <main className="flex h-screen w-full columns-2 bg-green-500">
-      <div className="flex-col min-h-screen bg-red-400 w-96 p-10 list-none overflow-auto">
+    <main className="flex h-screen w-full columns-2 dark ">
+      <div className="min-h-screen bg-background w-96 p-10 list-none overflow-auto">
         {hostData.map((host: Host, index: number) => {
           return (
             <h4
               key={host.addresses[0].addr}
               onClick={() => setSelectedHostIndex(index)}
               className={
-                "cursor-pointer" +
-                (selectedHostIndex === index ? " bg-blue-500" : "")
+                "cursor-pointer rounded" +
+                " " +
+                (selectedHostIndex === index
+                  ? "text-foreground"
+                  : "text-muted-foreground")
               }
             >
               {host.addresses[0].addr}
@@ -37,7 +40,7 @@ export default function Home() {
           );
         })}
       </div>
-      <div className="flex-col h-screen w-screen bg-blue-400 p-10 overflow-auto">
+      <div className="h-screen w-screen bg-background text-foreground p-10 overflow-auto">
         <HostNameTable host={hostData[selectedHostIndex]} />
         <PortsTable host={hostData[selectedHostIndex]} />
         <OSMatchesTable host={hostData[selectedHostIndex]} />
