d2c0f0650d80c28a7b001d5fd262cf9554b5d20c
HamnYce <hamadboa@gmail.com>
2024-07-29 01:32:23 +0300

    chore: Refactor PortsTable component to use tanstack/react-table

--- network_viewer_gui/src/components/host_tables/ports_table.tsx
+++ network_viewer_gui/src/components/host_tables/ports_table.tsx
@@ -2,39 +2,70 @@ import {
   Table,
   TableHeader,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableRow,
 } from "@/components/ui/table";
-import { Host } from "@/types/host_type";
+import { Host, Port } from "@/types/host_type";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
 type Props = {
-  readonly host: Host;
+  readonly ports: Port[];
 };
-export default function PortsTable({ host }: Props) {
+
+export default function PortsTable({ ports }: Props) {
+  const columnHelper = createColumnHelper<Port>();
+  const defaultColumns = [
+    columnHelper.accessor("id", {
+      header: "ID",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("protocol", {
+      header: "Protocol",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("service.name", {
+      header: "Service Name",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("state.state", {
+      header: "Port State",
+      cell: (info) => info.getValue(),
+    }),
+  ];
+
+  const table = useReactTable({
+    data: ports,
+    columns: defaultColumns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <Table>
-      <TableCaption>Ports Information</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead>ID</TableHead>
-          <TableHead>Protocol</TableHead>
-          <TableHead>Service Name</TableHead>
-          <TableHead>Port State</TableHead>
+          {table.getFlatHeaders().map((header) => (
+            <TableHead key={header.id}>
+              {flexRender(header.column.columnDef.header, header.getContext())}
+            </TableHead>
+          ))}
         </TableRow>
       </TableHeader>
       <TableBody>
-        {host.ports.map((port) => {
-          return (
-            <TableRow key={"" + host.addresses[0].addr + port.id}>
-              <TableCell>{port.id}</TableCell>
-              <TableCell>{port.protocol}</TableCell>
-              <TableCell>{port.service.name}</TableCell>
-              <TableCell>{port.state.state}</TableCell>
-            </TableRow>
-          );
-        })}
+        {table.getRowModel().rows.map((row) => (
+          <TableRow key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <TableCell key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
