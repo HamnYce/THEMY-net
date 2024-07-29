39220b7048247c8f39df305329c7be9ce5013a0c
HamnYce <hamadboa@gmail.com>
2024-07-29 11:52:39 +0300

    feat: add see more page

--- network_viewer_gui/src/app/page.tsx
+++ network_viewer_gui/src/app/page.tsx
@@ -11,36 +11,67 @@ import PortsTable from "@/components/host_tables/ports_table";
 export default function Home() {
   const [selectedHostIndex, setSelectedHostIndex] = useState(0);
   const [hostData, _] = useState([...data1, ...data2]);
+  const [seeMoreDetails, setSeeMoreDetails] = useState(false);
 
   return (
     <main className="flex h-screen w-full columns-2 dark ">
       <div className="min-h-screen bg-background w-96 p-10 list-none overflow-auto">
         {hostData.map((host: Host, index: number) => {
           return (
-            <h4
-              key={host.addresses[0].addr}
-              onClick={() => setSelectedHostIndex(index)}
-              className={
-                "cursor-pointer rounded" +
-                " " +
-                (selectedHostIndex === index
-                  ? "text-foreground"
-                  : "text-muted-foreground")
-              }
-            >
-              {host.addresses[0].addr}
-            </h4>
+            <div key={host.addresses[0].addr} className="flex flex-row">
+              <h4
+                onClick={() => {
+                  setSelectedHostIndex(index);
+                  setSeeMoreDetails(false);
+                }}
+                className={
+                  "cursor-pointer rounded" +
+                  " " +
+                  (selectedHostIndex === index
+                    ? "text-foreground"
+                    : "text-muted-foreground")
+                }
+              >
+                {host.addresses[0].addr}
+              </h4>
+              {selectedHostIndex === index && (
+                <>
+                  <div className="w-2"></div>
+                  <p
+                    className="text-foreground text-xs flex flex-col justify-center cursor-pointer"
+                    onClick={() => setSeeMoreDetails(!seeMoreDetails)}
+                  >
+                    see {seeMoreDetails ? "less" : "more"}
+                  </p>
+                </>
+              )}
+            </div>
           );
         })}
       </div>
       <div className="h-screen w-screen bg-background text-foreground p-10 overflow-auto">
-        <HostNamesTable
-          hostnames={hostData[selectedHostIndex].hostnames ?? []}
-        />
-        <PortsTable ports={hostData[selectedHostIndex].ports} />
-        <OSMatchesTable host={hostData[selectedHostIndex]} />
-        {/* // TODO: add addresses table */}
+        {seeMoreDetails ? (
+          <MoreDetailsPage host={hostData[selectedHostIndex]} />
+        ) : (
+          <OverView></OverView>
+        )}
       </div>
     </main>
+  );
+}
+
+function OverView() {
+  return <h1>Hello world</h1>;
+}
+
+function MoreDetailsPage({ host }: { host: Host }) {
+  return (
+    <>
+      <HostNamesTable hostnames={host.hostnames ?? []} />
+      <PortsTable ports={host.ports} />
+      {/* // TODO: convert osmatches table to tanstack table */}
+      <OSMatchesTable host={host} />
+      {/* // TODO: add addresses table */}
+    </>
   );
 }
