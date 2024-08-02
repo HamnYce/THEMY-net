174f5b6bedb4cdaf32b5a3ca2b427d7fefded919
HamnYce <hamadboa@gmail.com>
2024-08-03 01:33:38 +0300

    create CRUD handler skeleton

---
+++ go_server/handlers.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func CreateHostHandler(w http.ResponseWriter, r *http.Request) {}
+
+func RetrieveHostsHandler(w http.ResponseWriter, r *http.Request) {}
+
+func UpdateHostHandler(w http.ResponseWriter, r *http.Request) {}
+
+func DeleteHostHandler(w http.ResponseWriter, r *http.Request) {}
