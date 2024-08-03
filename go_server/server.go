f889e0e0613896d3dbcb79c92fa7595fb4867a73
HamnYce <hamadboa@gmail.com>
2024-08-03 23:02:57 +0300

    pluralize create host handler call

--- go_server/server.go
+++ go_server/server.go
@@ -36,7 +36,7 @@ func main() {
 	if DEBUG {
 		log.Println("attaching createHost Handler")
 	}
-	http.HandleFunc("/CreateHost", CreateHostHandler(db))
+	http.HandleFunc("/CreateHost", CreateHostsHandler(db))
 
 	if DEBUG {
 		log.Println("attaching RetrieveHosts Handler")
