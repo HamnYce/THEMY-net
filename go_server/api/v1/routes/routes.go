48dd6915a95285737b2cda8f651f74d52e170fd1
HamnYce <hamadboa@gmail.com>
2024-08-24 21:20:49 +0300

    migrate routing to chi router

--- go_server/api/v1/routes/routes.go
+++ go_server/api/v1/routes/routes.go
@@ -1,34 +1,51 @@
 package apiv1routes
 
 import (
-	"net/http"
-	utils "themynet"
 	handlers "themynet/api/v1/handlers"
-	middleware "themynet/api/v1/middleware"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
-func SetupRoutes() {
-	{
-		utils.DebugPrintf("attaching createHost Handler\n")
-		http.Handle("/CreateHosts", middleware.WrapperMiddleWare(http.HandlerFunc(handlers.CreateHostsHandler)))
-		utils.DebugPrintf("attached createHost Handler\n")
-	}
+func SetupRoutes() (r chi.Router) {
+	r = chi.NewRouter()
+	r.Use(
+		middleware.Logger,
+		middleware.AllowContentType("application/json"),
+		middleware.CleanPath,
+		middleware.RedirectSlashes,
+		middleware.Recoverer,
+		cors.Handler(
+			cors.Options{
+				// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+				AllowedOrigins: []string{"https://*", "http://*"},
+				// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+				AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+				AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+				ExposedHeaders:   []string{"Link"},
+				AllowCredentials: false,
+				MaxAge:           300, // Maximum value not ignored by any of major browsers
+			},
+		),
+	)
 
-	{
-		utils.DebugPrintf("attaching RetrieveHosts Handler\n")
-		http.Handle("/RetrieveHosts", middleware.WrapperMiddleWare(http.HandlerFunc(handlers.RetrieveHostsHandler)))
-		utils.DebugPrintf("attached RetrieveHosts Handler\n")
-	}
+	r.Post("/CreateHosts", handlers.CreateHostsHandler)
+	r.Post("/RetrieveHosts", handlers.RetrieveHostsHandler)
+	r.Post("/UpdateHosts", handlers.UpdateHostsHandler)
+	r.Post("/DeleteHosts", handlers.DeleteHostsHandler)
 
-	{
-		utils.DebugPrintf("attaching UpdateHost Handler\n")
-		http.Handle("/UpdateHosts", middleware.WrapperMiddleWare(http.HandlerFunc(handlers.UpdateHostsHandler)))
-		utils.DebugPrintf("attached UpdateHost Handler\n")
-	}
+	return r
 
-	{
-		utils.DebugPrintf("attaching DeleteHost Handler\n")
-		http.Handle("/DeleteHosts", middleware.WrapperMiddleWare(http.HandlerFunc(handlers.DeleteHostsHandler)))
-		utils.DebugPrintf("attached DeleteHost Handler\n")
-	}
+	// 	utils.DebugPrintf("attaching createHost Handler\n")
+	// 	utils.DebugPrintf("attached createHost Handler\n")
+
+	// 	utils.DebugPrintf("attaching RetrieveHosts Handler\n")
+	// 	utils.DebugPrintf("attached RetrieveHosts Handler\n")
+
+	// 	utils.DebugPrintf("attaching UpdateHost Handler\n")
+	// 	utils.DebugPrintf("attached UpdateHost Handler\n")
+
+	// 	utils.DebugPrintf("attaching DeleteHost Handler\n")
+	// 	utils.DebugPrintf("attached DeleteHost Handler\n")
 }
--- go_server/cmd/api/main.go
+++ go_server/cmd/api/main.go
@@ -7,10 +7,12 @@ import (
 	"themynet/internal/db"
 	datab "themynet/internal/db"
 
+	"github.com/go-chi/chi/v5"
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 func main() {
+
 	utils.DebugPrintf("Starting server with DEBUG on")
 	utils.RunConfig()
 	utils.DebugConfig()
@@ -19,11 +21,13 @@ func main() {
 	utils.CheckAndFatal(err)
 	defer db.DBSingleton().Close()
 
-	routes.SetupRoutes()
+	r := chi.NewRouter()
+	hostRouter := routes.SetupRoutes()
+
+	r.Mount("/api/v1", hostRouter)
 
 	utils.DebugPrintf("Listening on %s:%s\n", utils.GetHost(), utils.GetPort())
-
-	err = http.ListenAndServe(utils.GetHost()+":"+utils.GetPort(), nil)
+	err = http.ListenAndServe(utils.GetHost()+":"+utils.GetPort(), r)
 
 	utils.CheckAndFatal(err)
 }
